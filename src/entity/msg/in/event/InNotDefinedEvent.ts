f2a9f1e13f753c763688c45881c757af7bf502c6
Javen205 <javendev@126.com>
2019-11-03 19:52:37 +0800

    :recycle: 重构代码

--- src/entity/msg/in/event/InNotDefinedEvent.ts
+++
@@ -1,8 +0,0 @@
-import { EventInMsg } from "./EventInMsg";
-
-export class InNotDefinedEvent extends EventInMsg {
-
-    constructor(toUserName: string, fromUserName: string, createTime: number, event: string) {
-        super(toUserName, fromUserName, createTime, event);
-    }
-}
