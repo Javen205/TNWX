d8086d283e11085fba0328511813a2228b1e1d92
Javen205 <javendev@126.com>
2019-11-07 22:44:07 +0800

    :sparkles: 解耦accesstoken

---
+++ packages/accesstoken/src/cache/DefaultAccessTokenCache.ts
@@ -0,0 +1,23 @@
+/**
+ * @author Javen
+ * @copyright javendev@126.com
+ * @description 默认的缓存策略
+ */
+
+import { IAccessTokenCache } from './IAccessTokenCache';
+
+export class DefaultAccessTokenCache implements IAccessTokenCache {
+	private map: Map<string, string> = new Map<string, string>();
+
+	get(key: string): string {
+		return this.map.get(key) || '';
+	}
+
+	set(key: string, jsonValue: string) {
+		this.map.set(key, jsonValue);
+	}
+
+	remove(key: string) {
+		this.map.delete(key);
+	}
+}
