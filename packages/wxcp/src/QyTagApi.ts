a7d162e7509346f885a1aea13c8aee8deaeb6440
Javen205 <javendev@126.com>
2020-02-07 14:47:58 +0800

    :sparkles: 添加企业微信标签管理相关接口

---
+++ packages/wxcp/src/QyTagApi.ts
@@ -0,0 +1,124 @@
+import * as util from 'util'
+import { HttpKit } from '@tnwx/kits'
+import { AccessToken, QyAccessTokenApi } from '@tnwx/accesstoken'
+/**
+ * @author Javen
+ * @copyright javendev@126.com
+ * @description 标签管理相关接口
+ */
+export class QyTagApi {
+  private static createUrl: string = 'https://qyapi.weixin.qq.com/cgi-bin/tag/create?access_token=%s'
+
+  /**
+   * 创建标签
+   * @param tagName 标签名称
+   * @param tagId 标签id
+   */
+  public static async create(tagName: string, tagId: number) {
+    let accessToken: AccessToken = await QyAccessTokenApi.getAccessToken()
+    let url = util.format(this.createUrl, accessToken.getAccessToken)
+    return HttpKit.getHttpDelegate.httpPost(
+      url,
+      JSON.stringify({
+        tagname: tagName,
+        tagid: tagId
+      })
+    )
+  }
+
+  private static updateUrl: string = 'https://qyapi.weixin.qq.com/cgi-bin/tag/update?access_token=%s'
+
+  /**
+   * 更新标签名字
+   * @param tagName 标签名称
+   * @param tagId 标签id
+   */
+  public static async update(tagName: string, tagId: number) {
+    let accessToken: AccessToken = await QyAccessTokenApi.getAccessToken()
+    let url = util.format(this.updateUrl, accessToken.getAccessToken)
+    return HttpKit.getHttpDelegate.httpPost(
+      url,
+      JSON.stringify({
+        tagname: tagName,
+        tagid: tagId
+      })
+    )
+  }
+
+  private static deleteUrl: string = 'https://qyapi.weixin.qq.com/cgi-bin/tag/delete?access_token=%s&tagid=%s'
+
+  /**
+   * 删除标签
+   * @param tagId 标签id
+   */
+  public static async delete(tagId: number) {
+    let accessToken: AccessToken = await QyAccessTokenApi.getAccessToken()
+    let url = util.format(this.deleteUrl, accessToken.getAccessToken, tagId)
+    return HttpKit.getHttpDelegate.httpGet(url)
+  }
+
+  private static getUserUrl: string = 'https://qyapi.weixin.qq.com/cgi-bin/tag/get?access_token=%s&tagid=%s'
+
+  /**
+   * 获取标签成员
+   * @param tagId 标签id
+   */
+  public static async getUserByTagId(tagId: number) {
+    let accessToken: AccessToken = await QyAccessTokenApi.getAccessToken()
+    let url = util.format(this.getUserUrl, accessToken.getAccessToken, tagId)
+    return HttpKit.getHttpDelegate.httpGet(url)
+  }
+
+  private static addTagUsersUrl: string = 'https://qyapi.weixin.qq.com/cgi-bin/tag/addtagusers?access_token=%s'
+
+  /**
+   * 增加标签成员
+   * @param tagId 标签id
+   * @param userList 企业成员id列表
+   * @param partyList 企业部门id列表
+   */
+  public static async addTagUsers(tagId: number, userList?: Array<string>, partyList?: Array<number>) {
+    let accessToken: AccessToken = await QyAccessTokenApi.getAccessToken()
+    let url = util.format(this.addTagUsersUrl, accessToken.getAccessToken, tagId)
+    return HttpKit.getHttpDelegate.httpPost(
+      url,
+      JSON.stringify({
+        tagid: tagId,
+        userlist: userList,
+        partylist: partyList
+      })
+    )
+  }
+
+  private static delTagUsersUrl: string = 'https://qyapi.weixin.qq.com/cgi-bin/tag/deltagusers?access_token=%s'
+
+  /**
+   * 删除标签成员
+   * @param tagId 标签id
+   * @param userList 企业成员id列表
+   * @param partyList 企业部门id列表
+   */
+  public static async delTagUsers(tagId: number, userList?: Array<string>, partyList?: Array<number>) {
+    let accessToken: AccessToken = await QyAccessTokenApi.getAccessToken()
+    let url = util.format(this.delTagUsersUrl, accessToken.getAccessToken, tagId)
+    return HttpKit.getHttpDelegate.httpPost(
+      url,
+      JSON.stringify({
+        tagid: tagId,
+        userlist: userList,
+        partylist: partyList
+      })
+    )
+  }
+
+  private static getUrl: string = 'https://qyapi.weixin.qq.com/cgi-bin/tag/list?access_token=%s'
+
+  /**
+   * 获取标签列表
+   */
+  public static async getTagList() {
+    let accessToken: AccessToken = await QyAccessTokenApi.getAccessToken()
+    let url = util.format(this.getUrl, accessToken.getAccessToken)
+    return HttpKit.getHttpDelegate.httpGet(url)
+  }
+}
