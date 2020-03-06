0d4d92890dceaf598883c90866cb0b953b858f47
Javen205 <javendev@126.com>
2020-03-06 12:54:43 +0800

    :white_check_mark: 测试

--- packages/kits/__tests__/HttpKit.spec.ts
+++ packages/kits/__tests__/HttpKit.spec.ts
@@ -3,5 +3,5 @@ import { HttpKit } from './../src/HttpKit'
 test('http get', async () => {
   let url = 'https://gitee.com/api/v5/repos/Javen205/TNWX'
   let data = await HttpKit.getHttpDelegate.httpGet(url)
-  return expect(JSON.parse(data)['stargazers_count']).toBeGreaterThanOrEqual(117)
+  return expect(data['stargazers_count']).toBeGreaterThanOrEqual(117)
 })
