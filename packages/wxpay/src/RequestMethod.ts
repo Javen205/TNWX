70630610bb919f19d77ab0af6a1ffb4dc6c973ee
Javen205 <javendev@126.com>
2020-03-06 14:29:01 +0800

    :sparkles: 添加 HTTP 请求方法枚举

---
+++ packages/wxpay/src/RequestMethod.ts
@@ -0,0 +1,23 @@
+/**
+ * @author Javen
+ * @copyright javendev@126.com
+ * @description HTTP 请求的方法
+ */
+export enum RequestMethod {
+  /**
+   * 上传实质是 post 请求
+   */
+  UPLOAD = 'POST',
+  /**
+   * post 请求
+   */
+  POST = 'POST',
+  /**
+   * get 请求
+   */
+  GET = 'GET',
+  /**
+   * delete 请求
+   */
+  DELETE = 'DELETE'
+}
