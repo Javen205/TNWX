80c4297c64a037a32a31f0ed489369491b142deb
Javen205 <javendev@126.com>
2020-03-15 16:01:49 +0800

    添加 trycatch

--- sample/egg/app/middleware/jssdk.ts
+++ sample/egg/app/middleware/jssdk.ts
@@ -9,29 +9,34 @@ import * as uuid from 'uuid'
  */
 export default function jssdkMiddleWare(app: Application, options?: EggAppConfig['jssdk']): any {
   return async (ctx: Context, next: () => Promise<any>) => {
-    app.logger.info(`options:${JSON.stringify(options)}`)
+    try {
+      app.logger.info(`options:${JSON.stringify(options)}`)
 
-    const req = ctx.request
+      const req = ctx.request
 
-    let appId = ApiConfigKit.getApiConfig.getAppId
-    let timestamp = new Date().getTime()
-    let nonceStr = uuid.v1()
+      let appId = ApiConfigKit.getApiConfig.getAppId
+      let timestamp = new Date().getTime()
+      let nonceStr = uuid.v1()
 
-    let url: string = req.protocol
-      .concat('://')
-      .concat(req.host)
-      .replace(':80', '')
-      .concat(req.url)
+      let url: string = req.protocol
+        .concat('://')
+        .concat(req.host)
+        .replace(':80', '')
+        .concat(req.url)
 
-    ctx.app.logger.info(url)
+      ctx.app.logger.info(`url:${url}`)
 
-    let signature = await WeChat.jssdkSignature(nonceStr, String(timestamp), url)
-    req.body = {
-      appId: appId,
-      timestamp: timestamp,
-      nonceStr: nonceStr,
-      signature: signature
+      let signature = await WeChat.jssdkSignature(nonceStr, String(timestamp), url)
+
+      req.body = {
+        appId: appId,
+        timestamp: timestamp,
+        nonceStr: nonceStr,
+        signature: signature
+      }
+      await next()
+    } catch (error) {
+      console.log(error)
     }
-    await next()
   }
 }
--- sample/egg/app/middleware/qyjssdk.ts
+++ sample/egg/app/middleware/qyjssdk.ts
@@ -9,33 +9,37 @@ import * as uuid from 'uuid'
  */
 export default function qyJsSdkMiddleWare(app: Application, options?: EggAppConfig['qyjssdk']): any {
   return async (ctx: Context, next: () => Promise<any>) => {
-    app.logger.info(`options:${JSON.stringify(options)}`)
+    try {
+      app.logger.info(`options:${JSON.stringify(options)}`)
 
-    const req = ctx.request
+      const req = ctx.request
 
-    let appId = QyApiConfigKit.getApiConfig.getAppId
-    let corpId = QyApiConfigKit.getApiConfig.getCorpId
-    let timestamp = new Date().getTime().toString()
-    let nonceStr = uuid.v1()
+      let appId = QyApiConfigKit.getApiConfig.getAppId
+      let corpId = QyApiConfigKit.getApiConfig.getCorpId
+      let timestamp = new Date().getTime().toString()
+      let nonceStr = uuid.v1()
 
-    let url: string = req.protocol
-      .concat('://')
-      .concat(req.host)
-      .replace(':80', '')
-      .concat(req.url)
+      let url: string = req.protocol
+        .concat('://')
+        .concat(req.host)
+        .replace(':80', '')
+        .concat(req.url)
 
-    ctx.app.logger.info(url)
+      ctx.app.logger.info(url)
 
-    let signature = await QyWeChat.jssdkSignature(nonceStr, timestamp, url, QyJsApiType.CORP)
-    let qySignature = await QyWeChat.jssdkSignature(nonceStr, timestamp, url, QyJsApiType.AGENT)
-    req.body = {
-      appId: appId,
-      corpId: corpId,
-      timestamp: timestamp,
-      nonceStr: nonceStr,
-      signature: signature,
-      qySignature: qySignature
+      let signature = await QyWeChat.jssdkSignature(nonceStr, timestamp, url, QyJsApiType.CORP)
+      let qySignature = await QyWeChat.jssdkSignature(nonceStr, timestamp, url, QyJsApiType.AGENT)
+      req.body = {
+        appId: appId,
+        corpId: corpId,
+        timestamp: timestamp,
+        nonceStr: nonceStr,
+        signature: signature,
+        qySignature: qySignature
+      }
+      await next()
+    } catch (error) {
+      app.logger.error(error)
     }
-    await next()
   }
 }
